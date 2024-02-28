d82be8d502ad1c559ba2509a740f9e3fdd75ae8d
micuch11 <micuch1126@gmail.com>
2024-02-28 06:38:15 +0000

    Console App Array Assignment

---
+++ Console App Array Assignment/Console App Array Assignment/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Console_App_Array_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colorArray = { "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Black", "White", "Pink", "Brown" };
+            Console.WriteLine("Select a number between 0 and 9:");
+            int stringSelect = Convert.ToInt32(Console.ReadLine());
+            bool validString = false;
+
+            while (!validString)
+            {
+                try
+                {
+                    Console.WriteLine("Your favorite color is " + colorArray[stringSelect]);
+                    validString = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Sorry, that number selection is invalid. Please select a number between 0 and 9. ");
+                    stringSelect = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            List<string> occupationList = new List<string>()
+        {   "Data Analyst",
+            "UX Designer",
+            "Artist",
+            "Teacher",
+            "Lawyer",
+            "Cowboy",
+            "Athlete",
+            "Podcast Host",
+            "Software Developer",
+            "QA Tester"
+        };
+
+            Console.WriteLine("\nSelect another number between 0 and 9:");
+            int listSelect = Convert.ToInt32(Console.ReadLine());
+            bool validList = false;
+
+            while (!validList)
+            {
+                try
+                {
+                    Console.WriteLine("Your new occupation is: " + occupationList[listSelect]);
+                    validList = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Sorry, that number selection is invalid. Select a number between 0 and 9. ");
+                    listSelect = Convert.ToInt32(Console.ReadLine());        
+                }
+            }
+            }
+    }
+}
+
